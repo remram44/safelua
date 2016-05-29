92549b7ace4cde176d0bc2ff0009bf0fd776f407
Remi Rampin <remirampin@gmail.com>
2016-05-28 23:26:22 -0400

    Add debug trace to allocator

--- alloc.c
+++ alloc.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -23,12 +24,18 @@ struct Allocator *new_allocator(void)
     alloc->nb_blocks = 0;
     alloc->size_blocks = 4;
     alloc->total_allocated = 0;
+#ifdef _DEBUG_ALLOC
+    fprintf(stderr, "Created allocator %p\n", alloc);
+#endif
     return alloc;
 }
 
 void delete_allocator(struct Allocator *alloc)
 {
     size_t blk;
+#ifdef _DEBUG_ALLOC
+    fprintf(stderr, "Deleting allocator %p\n", alloc);
+#endif
     for(blk = 0; blk < alloc->nb_blocks; ++blk)
         free(alloc->blocks[blk].ptr);
     free(alloc->blocks);
@@ -39,10 +46,33 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
 {
     struct Allocator *alloc = ud;
     size_t blk;
+#ifdef _DEBUG_ALLOC
+    {
+        size_t i;
+        for(i = 0; i < alloc->nb_blocks; ++i)
+        {
+            fprintf(stderr, "%p %u ",
+                    alloc->blocks[i].ptr, alloc->blocks[i].size);
+            if(i % 4 == 0)
+                fprintf(stderr, "\n");
+        }
+        if(i % 4 != 1)
+            fprintf(stderr, "\n\n");
+        else
+            fprintf(stderr, "\n");
+    }
+    fprintf(stderr, "Lua request on allocator %p: "
+            "ptr=%p, osize=%u, nsize=%u\n", alloc, ptr, osize, nsize);
+#endif
     if(ptr == NULL)
     {
         if(nsize == 0)
+        {
+#ifdef _DEBUG_ALLOC
+            fprintf(stderr, "Returning NULL...\n");
+#endif
             return NULL;
+        }
         blk = alloc->nb_blocks;
         alloc->nb_blocks++;
         if(alloc->nb_blocks > alloc->size_blocks)
@@ -50,9 +80,16 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
             alloc->size_blocks *= 2;
             alloc->blocks = realloc(alloc->blocks,
                                     sizeof(struct Block) * alloc->size_blocks);
+#ifdef _DEBUG_ALLOC
+            fprintf(stderr, "Growing block table to %u blocks\n",
+                    alloc->size_blocks);
+#endif
         }
         alloc->blocks[blk].ptr = NULL;
         alloc->blocks[blk].size = 0;
+#ifdef _DEBUG_ALLOC
+        fprintf(stderr, "Allocated new block %u\n", blk);
+#endif
     }
     else
     {
@@ -67,6 +104,9 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
                 high = mid;
         }
         blk = low;
+#ifdef _DEBUG_ALLOC
+        fprintf(stderr, "Found block %u\n", blk);
+#endif
     }
     assert(alloc->blocks[blk].ptr == ptr &&
            (!ptr || alloc->blocks[blk].size == osize) );
@@ -77,11 +117,18 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
         memmove(&alloc->blocks[blk], &alloc->blocks[blk + 1],
                 sizeof(struct Block) * (alloc->nb_blocks - blk - 1));
         alloc->nb_blocks--;
+#ifdef _DEBUG_ALLOC
+        fprintf(stderr, "Removed block, now have %u blocks, %u bytes\n\n",
+                alloc->nb_blocks, alloc->total_allocated);
+#endif
         return NULL;
     }
     else
     {
         ptr = realloc(ptr, nsize);
+#ifdef _DEBUG_ALLOC
+        fprintf(stderr, "ptr=%p ", ptr);
+#endif
         alloc->blocks[blk].ptr = ptr;
         alloc->total_allocated += nsize - alloc->blocks[blk].size;
         alloc->blocks[blk].size = nsize;
@@ -92,6 +139,9 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
             alloc->blocks[blk] = alloc->blocks[blk - 1];
             alloc->blocks[blk - 1] = tmp;
             blk--;
+#ifdef _DEBUG_ALLOC
+            fprintf(stderr, "< ");
+#endif
         }
         while(blk + 1 < alloc->nb_blocks &&
               alloc->blocks[blk].ptr > alloc->blocks[blk + 1].ptr)
@@ -100,7 +150,15 @@ void *l_alloc(void *ud, void *ptr, size_t osize, size_t nsize)
             alloc->blocks[blk] = alloc->blocks[blk + 1];
             alloc->blocks[blk + 1] = tmp;
             blk++;
+#ifdef _DEBUG_ALLOC
+            fprintf(stderr, "> ");
+#endif
         }
+#ifdef _DEBUG_ALLOC
+        fprintf(stderr, "\n");
+        fprintf(stderr, "Moved block to %u, now have %u bytes\n\n",
+                blk, alloc->total_allocated);
+#endif
         return ptr;
     }
 }
