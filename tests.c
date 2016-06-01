95a455aeba0b5692f69d440379e7c0771fb3eb96
Remi Rampin <remirampin@gmail.com>
2016-06-01 18:26:09 -0400

    Test resource handler

--- tests.c
+++ tests.c
@@ -1,3 +1,4 @@
+#include <assert.h>
 #include <stdio.h>
 #include <lua.h>        /* Core Lua */
 #include <lauxlib.h>    /* Utility C functions */
@@ -11,10 +12,57 @@ int mycancel(lua_State *_state, void *ud)
     return --(*counter) <= 0;
 }
 
+char resources[10] = "          ";
+size_t resources_nb = 0;
+
+static void free_resource(int why, void *resource)
+{
+    assert(*(char*)resource != ' ');
+    fprintf(stderr, "Handler freed resource %d: %c\n",
+            (int)((char*)resource - resources), *(char*)resource);
+    *(char*)resource = ' ';
+}
+
+static int l_alloc(lua_State *state)
+{
+    size_t len;
+    const char *str;
+    if(lua_gettop(state) != 1)
+        luaL_error(state, "Expected a single argument");
+    str = luaL_checklstring (state, 1, &len);
+    if(len != 1)
+        luaL_error(state, "Expected 1-character string");
+    resources[resources_nb] = str[0];
+    lua_pushlightuserdata(state, &resources[resources_nb]);
+    fprintf(stderr, "Allocated resource %d: %c\n", (int)resources_nb, str[0]);
+    safelua_add_handler(state, free_resource, &resources[resources_nb]);
+    resources_nb++;
+    return 1;
+}
+
+static int l_free(lua_State *state)
+{
+    char *ptr;
+    if(lua_gettop(state) != 1)
+        luaL_error(state, "Expected a single argument");
+    ptr = lua_touserdata(state, 1);
+    if(ptr == NULL)
+        luaL_error(state, "Invalid resource");
+    assert(*ptr != ' ');
+    fprintf(stderr, "Freed resource %d: %c\n", (int)(ptr - resources), *ptr);
+    assert(safelua_remove_handler(state, free_resource, ptr) == 1);
+    *ptr = ' ';
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     lua_State *state = safelua_open();
     int i;
+    lua_pushcfunction(state, l_alloc);
+    lua_setglobal(state, "alloc");
+    lua_pushcfunction(state, l_free);
+    lua_setglobal(state, "free");
     for(i = 1; i < argc; ++i)
     {
         luaL_loadstring(state, argv[i]);
@@ -36,5 +84,6 @@ int main(int argc, char **argv)
         }
     }
     safelua_close(state);
+    fprintf(stderr, "Resources: \"%s\"\n", resources);
     return 0;
 }
